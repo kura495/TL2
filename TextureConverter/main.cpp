7455d09457f355bbc516338592463107827e969a
kura495 <k024g6001@g.neec.ac.jp>
2024-09-30 15:25:42 +0900

    コマンド引数の動きの確認

--- TextureConverter/main.cpp
+++ TextureConverter/main.cpp
@@ -1,9 +1,16 @@
 #include <cstdio>
 #include <cstdlib>
 
-int main() {
-	printf("Hello World!\n");
+int main(int argc,char* argv[]) {
 
+	//argcの数だけ繰り返す
+	for (int i = 0; i < argc;i++) {
+		//文字列argvのi番を表示
+		printf(argv[i]);
+		//改行
+		printf("\n");
+
+	}
 	system("pause");
 
 	return 0;
