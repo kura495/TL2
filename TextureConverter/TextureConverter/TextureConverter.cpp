e41a063fe3c45278c4c9c553b6ab80869423a5f8
k024g6001@g.neec.ac.jp <k024g6001@g.neec.ac.jp>
2024-11-25 14:22:13 +0900

    できた

--- TextureConverter/TextureConverter/TextureConverter.cpp
+++ TextureConverter/TextureConverter/TextureConverter.cpp
@@ -2,14 +2,28 @@
 
 void TextureConverter::ConvertTextureWICToDDS(const std::string& filePath)
 {
+	LoadWICTextureFromFile(filePath.c_str());
+
 }
 
 std::wstring TextureConverter::ConvertMultiByteStringToWideString(const std::string& mString)
 {
+	//ワイド文字列に変換した際の文字列を計算
 	int filePathBufferSize = MultiByteToWideChar(CP_ACP,0,mString.c_str(), -1,nullptr,0);
-	return std::wstring();
+
+	//ワイド文字列
+	std::wstring wString;
+	wString.resize(filePathBufferSize);
+
+	//ワイド文字列に変換
+	MultiByteToWideChar(CP_ACP,0,mString.c_str(),-1,&wString[0],filePathBufferSize);
+
+	return wString;
 }
 
 void TextureConverter::LoadWICTextureFromFile(const std::string& filePath)
 {
+	//ファイルパスをワイド文字列に変換する
+	std::wstring wfilePath = ConvertMultiByteStringToWideString(filePath.c_str());
+
 }
